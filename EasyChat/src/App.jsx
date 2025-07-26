dc699e2a5f217eca7de605a23b117e1a5b9b68c3
Shrey765 <shrey.eng72@gmail.com>
2025-07-27 00:43:46 +0530

    corrected the tailwind not working error

--- EasyChat/src/App.jsx
+++ EasyChat/src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
-import './App.css'
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function App() {
   
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomePage />
+    },
+    {
+      path: '/login',
+      element: <LoginPage />
+    },
+    {
+      path: '/profile',
+      element: <ProfilePage />
+    }
+  ])
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="bg-[url('./assets/bgImage.svg')] bg-contain text-white">
+      <RouterProvider router={router} />
+    </div>
+)
+
 }
 
 export default App
