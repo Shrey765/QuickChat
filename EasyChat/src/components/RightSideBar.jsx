f74825b58e3c761fe404855cece3d1bee0af4261
Shrey765 <shrey.eng72@gmail.com>
2025-08-12 09:47:56 +0530

    resolve the rightside profile pic and name bug

--- EasyChat/src/components/RightSideBar.jsx
+++ EasyChat/src/components/RightSideBar.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from "../../context/AuthContext";
 
 export default function RightSideBar() {
     const {selectedUser, message} = useContext(ChatContext);
-    const {logout, onlineUsers} = useContext(AuthContext);
+    const {logout, onlineUsers, authUser} = useContext(AuthContext);
     const [msgImages, setMsgImage] = useState([]);
 
     //Get all the images from the message and set them to state
@@ -20,13 +20,13 @@ export default function RightSideBar() {
     ${selectedUser ? "max-md:hidden" : ""}`}>
         <div className="pt-16 flex flex-col items-center gap-2 text-xs font-light 
         mx-auto">
-            <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" 
+            <img src={authUser?.profilePic || assets.avatar_icon} alt="" 
             className="w-20 aspect-[1/1] rounded-full"/>
             <h1 className="px-10 text-xl flex font-medium mx-auto items-center gap-2">
-                {onlineUsers.includes(selectedUser._id) && <p className="w-2 h-2 rounded-full bg-green-500"></p>}
-                {selectedUser.fullName}
+                {onlineUsers.includes(authUser._id) && <p className="w-2 h-2 rounded-full bg-green-500"></p>}
+                {authUser.fullName}
             </h1>
-            <p className="px-9 mx-auto">{selectedUser.bio}</p>
+            <p className="px-9 mx-auto">{authUser.bio}</p>
         </div>
         <hr className="border-[#ffffff50] my-4"/>
         <div className="px-5 text-xs">
